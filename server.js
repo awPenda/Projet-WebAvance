4987ff95cc753468de7bc6eb2a82aab29763dc96
bilal-62210 <bilal.benelkadi@viacesi.fr>
2023-06-29 15:50:52 +0200

    add cors

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const connectDB = require('./config/database'); // import database.js
 const notifRoutes = require('./routes/notifRoutes');
+const cors = require('cors');
 
 const app = express();  
+
 app.use(express.json({ extended: true }));
+app.use(cors());
 connectDB(); // call connectDB to establish connection with database
 
 app.use('/api', notifRoutes);
