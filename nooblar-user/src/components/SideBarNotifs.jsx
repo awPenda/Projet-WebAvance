9a2a0df1fb5808e05dea11cab94b9f59811064ea
bilal-62210 <bilal.benelkadi@viacesi.fr>
2023-06-29 14:28:38 +0200

    this commit add the getnotifs

--- nooblar-user/src/components/SideBarNotifs.jsx
+++ nooblar-user/src/components/SideBarNotifs.jsx
@@ -2,98 +2,32 @@ import NotifBox from './NotifBox';
 
 import Icons from './Icons';
 import { toggleSideBarNotifs } from '../Global'
+import axios from 'axios';
 
-
-const user_notifs = [
-  {
-    id: 0,
-    priority: "1",
-    title: "title of urgent notification",
-    body: "body of notification",
-  },
-  {
-    id: 1,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 2,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 3,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 4,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 5,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 6,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 13,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 12,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 7,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 8,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 9,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 10,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-  {
-    id: 11,
-    priority: "0",
-    title: "title of notification",
-    body: "body of notification",
-  },
-]
 
 
 export default function SideBarNotifs(user_data, hidden) {
   let listNotifs = [];
+  let user_notifs = [];
+const par = localStorage.getItem('id');
+let par2={user_id:par};
+
+
+axios
+                .get('http://localhost:8070/api/getAllNotif', { params: par2  })
+                .then((response) => {
+                    //console.log(response.data);
+                    // displayFetchedUsers(response.data);
+                    console.log(response.data);
+                    const hmtl_elm = document.getElementById('div-notif');
+                    console.log(hmtl_elm);
+                    hmtl_elm.setAttribute('data-listenotifs', JSON.stringify(response.data));
+                    //console.log('Successfully fetched user data');
+                })
+                .catch((error) => {
+                    console.error('Error fetching user data:', error);
+                });
+
   return (
     <div id="SideBarNotifs" className="SideBar-notifs SideBar" style={{ visibility: hidden ? "hidden" : "visible" }}>
       {/* Notifications */}
@@ -105,7 +39,7 @@ export default function SideBarNotifs(user_data, hidden) {
         <Icons type="notif_ico" />
       </div>
 
-      <div className='SideBar-notifList'>
+      <div className='SideBar-notifList' id='div-notif' data-listnotifs>
         {user_notifs.forEach((notif) => {
           listNotifs.push(
             <NotifBox key={notif.id} id={notif.id} priority={notif.priority} title={notif.title} body={notif.body} />
