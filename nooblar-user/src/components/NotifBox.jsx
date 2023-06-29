f84e3ed3ee5820c7fc1d359f16b0831b27431c55
awPenda <amelie.willems2@gmail.com>
2023-06-29 16:31:51 +0200

    add notif display

--- nooblar-user/src/components/NotifBox.jsx
+++ nooblar-user/src/components/NotifBox.jsx
@@ -1,7 +1,7 @@
 import Icons from './Icons';
-import {expand} from '../Global';
+import { expand } from '../Global';
 
-export default function NotifBox({ id, priority, title, body }) {
+export default function NotifBox({ id, priority, title, body, url }) {
   function Icon({ priority }) {
     if (priority === "0") {
       return (<Icons type={"info_ico"} />);
@@ -18,17 +18,26 @@ export default function NotifBox({ id, priority, title, body }) {
 
   const key_name = "Notif" + id.toString();
 
-  return (
-  <button type="button" onClick={(e) => expand(e,key_name)}  key={key_name} id={key_name} className={"NotifBox " + key_name + " " + BorderColor({ priority })}>
-    <div>
-      <Icon priority={priority} />
-        {title}
-      <Icons type={"trash_ico"} />
-    </div>
-    <div id={`notif_body${key_name}`} className={`notif_body ${key_name}`} style={{display:"none"}}>
-      {body}
-    </div>
 
-  </button>
+  function redirectlink() {
+    window.location.href = url;
+
+  }
+
+  return (
+    <button type="button" onClick={(e) => expand(e, key_name)} key={key_name} id={key_name} className={"NotifBox " + key_name + " " + BorderColor({ priority })}>
+      <div >
+        <Icon priority={priority} />
+        {title}
+        <button onClick={redirectlink} className='main_color_btn'>
+          url
+        </button>
+        <Icons type={"trash_ico"} />
+      </div>
+      <div id={`notif_body${key_name}`} className={`notif_body ${key_name}`} style={{ display: "none" }}>
+        {body}
+      </div>
+
+    </button>
   )
 }
--- nooblar-user/src/components/SideBarNotifs.jsx
+++ nooblar-user/src/components/SideBarNotifs.jsx
@@ -1,32 +1,53 @@
 import NotifBox from './NotifBox';
-
 import Icons from './Icons';
 import { toggleSideBarNotifs } from '../Global'
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 
+function getUsersNotifs() {
+  const hmtl_elm = document.getElementById('div-notif');
+  const test = hmtl_elm.dataset.listenotifs;
+  let list = JSON.parse(test);
 
+  return list;
+}
 
 export default function SideBarNotifs(user_data, hidden) {
+  //use state false
   let listNotifs = [];
-  let user_notifs = [];
-const par = localStorage.getItem('id');
-let par2={user_id:par};
+  let user_notifs = [];//= getUsersNotifs();
+  const [userslist, setUserslist] = useState(user_notifs);
+
+  const par = localStorage.getItem('id');
+  let par2 = { user_id: par };
 
 
-axios
-                .get('http://localhost:8070/api/getAllNotif', { params: par2  })
-                .then((response) => {
-                    //console.log(response.data);
-                    // displayFetchedUsers(response.data);
-                    console.log(response.data);
-                    const hmtl_elm = document.getElementById('div-notif');
-                    console.log(hmtl_elm);
-                    hmtl_elm.setAttribute('data-listenotifs', JSON.stringify(response.data));
-                    //console.log('Successfully fetched user data');
-                })
-                .catch((error) => {
-                    console.error('Error fetching user data:', error);
-                });
+  axios
+    .get('http://localhost:8070/api/getAllNotif', { params: par2 })
+    .then((response) => {
+      //console.log(response.data);
+      // displayFetchedUsers(response.data);
+      console.log(response.data);
+      const hmtl_elm = document.getElementById('div-notif');
+      console.log(hmtl_elm);
+
+      localStorage.setItem('notifs', JSON.stringify(response.data))
+      setUserslist(response.data);
+
+      hmtl_elm.setAttribute('data-listenotifs', JSON.stringify(response.data));
+      user_notifs = getUsersNotifs();
+
+      // useEffect(() => { dorenderListnotifs(true) }, []);
+      //console.log('Successfully fetched user data');
+    })
+    .catch((error) => {
+      console.error('Error fetching user data:', error);
+    });
+
+
+  //render calendar after dom has finished rendering components
+  // const [listnotifs, dorenderListnotifs] = useState(false);
+
 
   return (
     <div id="SideBarNotifs" className="SideBar-notifs SideBar" style={{ visibility: hidden ? "hidden" : "visible" }}>
@@ -39,12 +60,17 @@ axios
         <Icons type="notif_ico" />
       </div>
 
+
+      {/* if use stage */}
       <div className='SideBar-notifList' id='div-notif' data-listnotifs>
-        {user_notifs.forEach((notif) => {
+
+
+        {userslist.forEach((notif) => {
           listNotifs.push(
-            <NotifBox key={notif.id} id={notif.id} priority={notif.priority} title={notif.title} body={notif.body} />
+            <NotifBox key={notif.id} id={notif.id} priority={notif.priority} title={notif.title} body={notif.body} url={notif.url} />
           )
         })}
+
         {listNotifs}
       </div>
       <div>
